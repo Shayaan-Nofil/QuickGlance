476616efdf201fc59ca5a42969d1f60b6fe33c8a
Shayaan Nofil <i210450@nu.edu.pk>
2024-04-18 15:57:30 +0500

    Reset Password and OTP

---
+++ app/src/main/java/com/m_abdullah/quickglance/reset_password_Activity.kt
@@ -0,0 +1,11 @@
+package com.m_abdullah.quickglance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reset_password_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
---
+++ app/src/main/java/com/m_abdullah/quickglance/reset_password_number_activity.kt
@@ -0,0 +1,11 @@
+package com.m_abdullah.quickglance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reset_password_number_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password_number)
+    }
+}
