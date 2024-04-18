42f87a9737bc719ae013a17efd3e7f6d1da1fd0c
Shayaan Nofil <i210450@nu.edu.pk>
2024-04-18 18:26:19 +0500

    Credentials page

---
+++ app/src/main/java/com/m_abdullah/quickglance/Credentials_Activity.kt
@@ -0,0 +1,16 @@
+package com.m_abdullah.quickglance
+
+import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.m_abdullah.quickglance.R
+import java.util.*
+
+class Credentials_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credentials)
+
+    }
+}
